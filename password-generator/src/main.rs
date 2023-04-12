db2899130b90ae15ac9646d37576b3a912c96b6b
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-12 02:14:02 -0700

    Create password generator startup and teardown

--- password-generator/src/main.rs
+++ password-generator/src/main.rs
@@ -1,3 +1,39 @@
+use std::io::{stdout, stdin, Write};
+use std::thread::sleep;
+use std::time::Duration;
+use termion::raw::IntoRawMode;
+use termion::input::TermRead;
+use termion::clear;
+use termion::color;
+use termion::cursor::{Goto, Show, Left, BlinkingUnderline};
+use termion::event::Key;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    write!(
+        stdout,
+        "{}{}Welcome to the password generator!{}{}",
+        clear::All,
+        Goto(1,1),
+        Show,
+        BlinkingUnderline,
+    ).unwrap();
+
+    let mut running = true;
+    while running {
+        if !running{ break; }
+    }
+
+    write!(
+        stdout,
+        "{}{}{}...password generator shutting down.{}",
+        clear::All,
+        Goto(1,1),
+        color::Fg(color::Green),
+        color::Fg(color::Reset)
+    ).unwrap();
+    stdout.flush().unwrap();
+
+    sleep(Duration::from_secs(2));
+    return;
 }
