f0260ae72994a64144917d034483faafb021935d
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-22 00:53:09 -0700

    Update implementation for get password length

--- password-generator/src/main.rs
+++ password-generator/src/main.rs
@@ -28,55 +28,89 @@ fn main() {
     let mut running = true;
     while running {
         let stdin = stdin();
-        write!(stdout,
-            "How many digits would you like your password to be?\n\r{}> {}",
-            color::Fg(color::Red),
-            color::Fg(color::Reset),
-        ).unwrap();
-        stdout.flush().unwrap();
+        // Get the desired password length
+        let mut length_prompt = true;
+        while length_prompt{
+            write!(stdout,
+                "{}{}How many digits would you like your password to be? (8-60)\n\r{}> {}",
+                clear::All,
+                Goto(1,1),
+                color::Fg(color::Red),
+                color::Fg(color::Reset),
+            ).unwrap();
+            stdout.flush().unwrap();
 
-        let mut input = String::new();
-        for key in stdin.keys(){
-            stdout.flush().unwrap();
-            match key.unwrap() {
-                Key::Char('q') | Key::Esc => {
-                    running = false;
-                    break;
-                },
-                Key::Char('\n') | Key::Char(' ') => {
-                    if let Ok(num) = eval_length(input) {
-                        let password = generate_pasword(num);
+            let mut length = String::new();
+            for key in stdin.keys(){
+                stdout.flush().unwrap();
+                match key.unwrap() {
+                    Key::Char('q') | Key::Esc => {
+                        running = false;
+                        length_prompt = false;
+                        break;
+                    },
+                    Key::Char('\n') | Key::Char(' ') => {
+                        if let Ok(num) = eval_length(&length) {
+                            length_prompt = false;
+                            break;
+                    } else{
+                            write!(
+                                stdout,
+                                "\n\r{}Length unable to be implemented.{}\n\n\r",
+                                color::Fg(color::Red),
+                                color::Fg(color::Reset),
+                            ).unwrap();
+                            stdout.flush().unwrap();
+                        }
+                        sleep(Duration::from_millis(800));
+                        break;
+                    },
+                    Key::Char(c) => {
+                        length.push(c);
                         write!(
                             stdout,
-                            "\n\rYour generated password is: {}{}{} \n\n\r",
-                            color::Fg(color::Green),
-                            password,
-                            color::Fg(color::Reset),
+                            "{}", c,
                         ).unwrap();
                         stdout.flush().unwrap();
-                } else{
-                        write!(
-                            stdout,
-                            "\n\r{}Length unable to be implemented.{}\n\n\r",
-                            color::Fg(color::Red),
-                            color::Fg(color::Reset),
-                        ).unwrap();
-                        stdout.flush().unwrap();
+                    },
+                    _ => {},
+                }
+                if let Ok(num) = eval_length(&length){
+                    match num {
+                        0..=10 | 61.. => {
+                            write!(
+                                stdout,
+                                "\r{}{}{}{}",
+                                clear::AfterCursor,
+                                color::Fg(color::Red),
+                                length,
+                                color::Fg(color::Reset)
+                            ).unwrap();
+                        },
+                        11..=15 => {
+                            write!(
+                                stdout,
+                                "\r{}{}{}{}",
+                                clear::AfterCursor,
+                                color::Fg(color::Yellow),
+                                length,
+                                color::Fg(color::Reset)
+                            ).unwrap();
+                        },
+                        16..=60 => {
+                            write!(
+                                stdout,
+                                "\r{}{}{}{}",
+                                clear::AfterCursor,
+                                color::Fg(color::Green),
+                                length,
+                                color::Fg(color::Reset)
+                            ).unwrap();
+                        }
                     }
-                    sleep(Duration::from_secs(1));
-                    break;
-                },
-                Key::Char(c) => {
-                    input.push(c);
-                    write!(
-                        stdout,
-                        "{}", c,
-                    ).unwrap();
-                    stdout.flush().unwrap();
-                },
-                _ => {},
+                }
+                stdout.flush().unwrap();
             }
-            stdout.flush().unwrap();
         }
         if !running{ break; }
     }
@@ -95,7 +129,7 @@ fn main() {
     return;
 }
 
-fn eval_length(length: String) -> Result<u32, ()>{
+fn eval_length(length: &str) -> Result<u32, ()>{
     if let Ok(num) = length.trim().parse::<u32>(){
         if num < 61 && num > 7{
             Ok(num)
@@ -107,10 +141,10 @@ fn eval_length(length: String) -> Result<u32, ()>{
     }
 }
 
-fn generate_pasword(length: u32) -> String {
+fn generate_pasword(length: u32, root: &str) -> String {
     let mut rng = rand::thread_rng();
     let mut password = String::new();
-    for _ in 0..length{
+    for _ in 0..(length - root.len() as u32){
         match rng.gen_range(0..120) % 4 {
             0 => {
                 password.push(
