5d89d41bf694c9ae2fa8eb1c48b17ca125888d99
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-22 21:33:56 -0700

    Implement opening and closing deialog for development timer

--- development-timer/src/main.rs
+++ development-timer/src/main.rs
@@ -1,5 +1,64 @@
-// stopwatch, timer, time tracker, recent timed events, usage log
+use std::io::{stdin, stdout, Write};
+use std::thread::sleep;
+use std::time::Duration;
+use termion::color;
+use termion::clear;
+use termion::cursor;
 
+// stopwatch, work interval, timer, alarms
 fn main() {
-    println!("Hello, Development Timer!");
+    let mut stdout = stdout();
+    write!(
+        stdout,
+        "{}{}{}Welcome to the Development Timer!{}",
+        clear::All,
+        cursor::Goto(1, 1),
+        color::Fg(color::Green),
+        color::Fg(color::Reset)
+    ).unwrap();
+    stdout.flush().unwrap();
+    sleep(Duration::from_millis(1500));
+
+    let mut running = true;
+    while running {
+        let stdin = stdin();
+        write!(
+            stdout,
+            "{}{}Doing timer things",
+            clear::All,
+            cursor::Goto(1,1),
+        ).unwrap();
+        stdout.flush().unwrap();
+        sleep(Duration::from_millis(800));
+        running = false;
+    }
+
+    write!(
+        stdout,
+        "{}{}{}{}Closing up Development Timer..",
+        clear::All,
+        cursor::Goto(1, 1),
+        color::Fg(color::Red),
+        cursor::Hide,
+    ).unwrap();
+    for _ in 0..4{
+        write!(
+            stdout,
+            "..."
+        ).unwrap();
+        stdout.flush().unwrap();
+        sleep(Duration::from_millis(200));
+    }
+    write!(
+        stdout,
+        "...{}DEVELOPERS OUT!{}",
+        color::Fg(color::Green),
+        color::Fg(color::Reset),
+    ).unwrap();
+    stdout.flush().unwrap();
+    sleep(Duration::from_millis(500));
 }
+
+
+
+
