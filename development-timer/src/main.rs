b7301298538d7bdceb0d17f469d16654e67fb6f4
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-23 13:19:16 -0700

    Add program selection dialog to get user input on what they want to do in the timer

--- development-timer/src/main.rs
+++ development-timer/src/main.rs
@@ -4,6 +4,8 @@ use std::time::Duration;
 use termion::color;
 use termion::clear;
 use termion::cursor;
+use termion::event::Key;
+use termion::input::TermRead;
 
 // stopwatch, work interval, timer, alarms
 fn main() {
@@ -22,6 +24,125 @@ fn main() {
     let mut running = true;
     while running {
         let stdin = stdin();
+
+        write!(
+            stdout,
+            "{}{}{}How can I assist?{}{}",
+            clear::All,
+            cursor::Goto(1,1),
+            color::Fg(color::Cyan),
+            color::Fg(color::Reset),
+            cursor::Hide
+        ).unwrap();
+        let options: [&str; 4] = [
+            "Stopwatch",
+            "Work Intervals",
+            "Timer",
+            "Alarm"
+        ];
+        let mut selected: u32 = 0;
+        for (idx, option) in options.iter().enumerate(){
+            if idx == selected as usize {
+                write!(
+                    stdout,
+                    "{}{}{}  > {}{}",
+                    cursor::Goto(1, idx as u16 + 2),
+                    clear::CurrentLine,
+                    color::Fg(color::Red),
+                    option.to_uppercase(),
+                    color::Fg(color::Reset),
+                ).unwrap();
+            } else {
+                write!(
+                    stdout,
+                    "{}{}{}    {}",
+                    cursor::Goto(1, idx as u16 + 2),
+                    clear::CurrentLine,
+                    color::Fg(color::Reset),
+                    option,
+                ).unwrap();
+            }
+            stdout.flush().unwrap();
+        }
+
+        for key in stdin.keys(){
+            match key.unwrap(){
+                Key::Esc | Key::Char('q') => {
+                    running = false;
+                    break;
+                },
+                Key::Char(' ') | Key::Char('\n') => {
+                    match selected {
+                        0 => {
+
+                        },
+                        1 => {
+
+                        },
+                        2 => {
+
+                        },
+                        _ => {
+
+                        }
+                    }
+                    write!(
+                        stdout,
+                        "{}Opening: {}{}{}",
+                        cursor::Goto(1, options.len() as u16 + 3),
+                        color::Fg(color::Green),
+                        options[selected as usize],
+                        color::Fg(color::Reset),
+                    ).unwrap();
+                    break;
+                },
+                Key::Up => {
+                    if selected > 0 {
+                        write!(
+                            stdout,
+                            "{}{}{}    {}",
+                            clear::CurrentLine,
+                            cursor::Goto(1, selected as u16 + 2),
+                            color::Fg(color::Reset),
+                            options[selected as usize],
+                        ).unwrap();
+                        selected -= 1;
+                        write!(
+                            stdout,
+                            "{}{}{}  > {}",
+                            clear::CurrentLine,
+                            cursor::Goto(1, selected as u16 + 2),
+                            color::Fg(color::Reset),
+                            options[selected as usize].to_uppercase(),
+                        ).unwrap();
+                    }
+                },
+                Key::Down => {
+                    if (selected as usize) < options.len() - 1 {
+                        write!(
+                            stdout,
+                            "{}{}{}    {}",
+                            clear::CurrentLine,
+                            cursor::Goto(1, selected as u16 + 2),
+                            color::Fg(color::Reset),
+                            options[selected as usize],
+                        ).unwrap();
+                        selected += 1;
+                        write!(
+                            stdout,
+                            "{}{}{}  > {}",
+                            clear::CurrentLine,
+                            cursor::Goto(1, selected as u16 + 2),
+                            color::Fg(color::Reset),
+                            options[selected as usize].to_uppercase(),
+                        ).unwrap();
+                    }
+                },
+                _ => {}
+            }
+            stdout.flush().unwrap();
+        }
+
         write!(
             stdout,
             "{}{}Doing timer things",
