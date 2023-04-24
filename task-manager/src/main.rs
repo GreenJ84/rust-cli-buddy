325b16a6ce5dcd2ef76e9d4c3a5d570ea51a2cbf
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-24 14:27:54 -0700

    Build main Task manage open / close dialog

--- task-manager/src/main.rs
+++ task-manager/src/main.rs
@@ -1,4 +1,83 @@
+use std::io::{stdin, stdout, Write};
+use std::thread::sleep;
+use std::time::Duration;
+use termion::clear;
+use termion::color;
+use termion::cursor;
+use termion::style;
+use termion::input::TermRead;
+
 // Add, manage, delete tasks. task timelines, deadlines, and updates
 fn main() {
-    println!("Hello, Task Manager!");
+    let mut stdout = stdout();
+    write!(
+        stdout,
+        "{}{}{}Welcome to the Task Manager!{}",
+        clear::All,
+        cursor::Goto(1,1),
+        color::Fg(color::Green),
+        color::Fg(color::Reset),
+    ).unwrap();
+
+    let mut running = false;
+    let first = true;
+    while running{
+        let stdin = stdin();
+        write!(
+            stdout,
+            "{}{}{}How can I help you {}?{}\n\r",
+            clear::All,
+            cursor::Goto(1,1),
+            color::Fg(color::Cyan),
+            if first {"today"} else {"next"},
+            color::Fg(color::Reset),
+        ).unwrap();
+
+        let options: [&str; 4] = [
+            "'N'ew Task",
+            "'R'eview Task",
+            "'U'pdate Task",
+            "'D'elete Task",
+        ];
+
+        let mut selected = 0;
+        for (idx, option) in options.iter().enumerate(){
+            if selected == idx{
+                write!(
+                    stdout,
+                    "",
+                    color::Fg(color::Red),
+                    option
+                ).unwrap();
+            } else{
+                write!(
+                    stdout,
+                    "",
+                    color::Fg(color::Red),
+                    option,
+                ).unwrap();
+            }
+        }
+
+        first = false;
+    }
+
+    write!(
+        stdout,
+        "Closing Task Manager..."
+    ).unwrap();
+    for _ in 0..5{
+        write!(
+            stdout,
+            ".."
+        ).unwrap();
+        stdout.flush().unwrap();
+        sleep(Duration::from_millis(100));
+    }
+    write!(
+        stdout,
+        "...BYE",
+    ).unwrap();
+    stdout.flush().unwrap();
+    sleep(Duration::from_millis(500));
 }
