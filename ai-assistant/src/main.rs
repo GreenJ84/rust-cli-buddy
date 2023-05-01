fc32f183118dd42957cc5bfa6d9335760cd3cb55
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-30 23:03:14 -0700

    Implement the chat connection in seperate functionality

--- ai-assistant/src/main.rs
+++ ai-assistant/src/main.rs
@@ -1,5 +1,35 @@
-// Development companion, AI assistant
+mod ai_structs;
 
+use reqwest::blocking::Client;
+use serde::{Deserialize, Serialize};
+
+use ai_structs::{CompletionRequest, CompletionResponse, CompletionChoices, LogProbs};
+
+// Development companion, AI assistant
 fn main() {
-    println!("Hello, AI Co-Pilot!");
+
+}
+
+
+fn interact_with_chat_ai(mesage: &str) -> String {
+    let client = Client::new();
+    let request = CompletionRequest {
+        prompt: message.to_owned(),
+        temperature: 0.7,
+        max_tokens: 60,
+        n: 1,
+        stop: None,
+        engine: "davinci".to_owned(),
+    };
+
+    let response = client
+    .post("https://api.openai.com/v1/engines/davinci/completions")
+    .header("Content-Type", "application/json"),
+    .header("Authorization", format!("Bearer {}", TOKEN))
+    .json(&request)
+    .send()
+    .unwrap();
+
+    let response_json: CompletionResponse = response.json().unwrap();
+    response_json.choices[0].text.clone()
 }
