2595be04ff9e284ba4bc2150c42aa0ad49900b1f
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-03 00:13:03 -0700

    Complete Chat GPT connection and update response rendering

--- ai-assistant/src/main.rs
+++ ai-assistant/src/main.rs
@@ -4,16 +4,15 @@ mod ai_structs;
 use dotenv::dotenv;
 use reqwest::Client;
 use reqwest::header::{ACCEPT, CONTENT_TYPE, AUTHORIZATION};
-use serde::{Deserialize, Serialize};
 use std::io::{Write, stdin, stdout};
 use std::thread::sleep;
 use std::time::Duration;
-use termion::{color, cursor, clear, terminal_size};
+use termion::{color, cursor, clear, style, terminal_size};
 use termion::event::Key;
 use termion::cursor::DetectCursorPos;
 use termion::input::TermRead;
 
-use ai_structs::{CompletionRequest, CompletionResponse, CompletionChoices, LogProbs};
+use ai_structs::{CompletionRequest, CompletionResponse, Message};
 
 // Development companion, AI assistant
 #[tokio::main]
@@ -48,7 +47,7 @@ async fn main() {
 
         let mut input = String::new();
         for key in stdin().keys(){
-            let position = stdout.cursor_pos().unwrap();
+            let _position = stdout.cursor_pos().unwrap();
             let _terminal = terminal_size().unwrap();
             match key.unwrap(){
                 Key::Esc => {
@@ -56,46 +55,89 @@ async fn main() {
                     break;
                 },
                 Key::Delete | Key::Backspace => {
-                    if input.len() > 0{
+                    if input.len() > 0 {
                         input.pop();
                         write!(
                             stdout,
-                            "{}",
-                            "\x08"
+                            "\x08 \x08",
                         ).unwrap()
                     }
                 },
                 Key::Char('\n') => {
-                    let response = interact_with_chat_ai(&input).await;
-                    write!(
-                        stdout,
-                        "{}{}{:?}{}\n\r",
-                        cursor::Goto(1, position.1 + 4),
-                        color::Fg(color::Cyan),
-                        response,
-                        color::Fg(color::Reset),
-                    ).unwrap();
+                    write!(stdout,"\n\n\r").unwrap();
+                    stdout.flush().unwrap();
+                    let mut offset: String = String::new();
+                    match interact_with_chat_ai(&input).await{
+                        Ok(response) => {
+                            write!(stdout, "{}", color::Fg(color::Green)).unwrap();
+                            stdout.flush().unwrap();
+                            let mut in_code = false;
+                            for line in response.split("\n"){
+                                if line.starts_with("```"){
+                                    in_code = !in_code;
+                                    if in_code {
+                                        write!(
+                                            stdout,
+                                            "{}{}{}{}\n\r",
+                                            color::Fg(color::LightMagenta),
+                                            style::Bold,
+                                            line,
+                                            color::Fg(color::LightBlue)
+                                        ).unwrap();
+                                        offset = String::from("\t"); 
+                                    } else {
+                                        offset = String::new();
+                                        write!(
+                                            stdout,
+                                            "{}{}{}{}\n\r",
+                                            color::Fg(color::LightMagenta),
+                                            line,
+                                            style::Reset,
+                                            color::Fg(color::Green),
+                                        ).unwrap();
+                                    }
+                                } else{
+                                    write!(
+                                        stdout,
+                                        "{}{}\n\r",
+                                        offset,
+                                        line,
+                                    ).unwrap();
+                                }
+                                stdout.flush().unwrap();
+                            }
+                        },
+                        Err(problem) => 
+                            write!(
+                                stdout, 
+                                "{}{}{}",
+                                color::Fg(color::Red),
+                                problem,
+                                color::Fg(color::Reset),
+                            ).unwrap(),
+                    }
+                    write!(stdout,"\n\n\r").unwrap();
+                    stdout.flush().unwrap();
                     break;
                 },
-                Key::Left => {
-                    if position.1 > 1 && position.0 > 4 {
-                        write!(
-                            stdout,
-                            "{}",
-                            cursor::Left(1)
-                        ).unwrap();
-                    }
-                },
-                Key::Right => {
-                    // write!(stdout, "{:?}", position).unwrap();
-                    if stdout.cursor_pos().unwrap().0 - 4 < input.len() as u16 {
-                        write!(
-                            stdout, 
-                            "{}", 
-                            cursor::Right(1)
-                        ).unwrap();
-                    }
-                },
+                // Key::Left => {
+                //     if position.1 > 1 && position.0 > 4 {
+                //         write!(
+                //             stdout,
+                //             "{}",
+                //             cursor::Left(1)
+                //         ).unwrap();
+                //     }
+                // },
+                // Key::Right => {
+                //     if stdout.cursor_pos().unwrap().0 - 4 < input.len() as u16 {
+                //         write!(
+                //             stdout, 
+                //             "{}", 
+                //             cursor::Right(1)
+                //         ).unwrap();
+                //     }
+                // },
                 Key::Char(c) => {
                     input.push(c);
                     write!(
@@ -111,7 +153,7 @@ async fn main() {
         if running{
             write!(
                 stdout,
-                "{}Is there another question I can help with?\n\r {} > {}",
+                "{}Is there more I can help with? y/n\n\r {} > {}",
                 color::Fg(color::Yellow),
                 color::Fg(color::Red),
                 color::Fg(color::Reset),
@@ -127,7 +169,22 @@ async fn main() {
                     Key::Char('y') | Key::Char('\n') => {
                         break;
                     },
-                    _ => {}
+                    _ => {
+                        write!(
+                            stdout,
+                            "{}Not understood. Try again{}",
+                            color::Fg(color::Yellow),
+                            color::Fg(color::Reset),
+                        ).unwrap();
+                        stdout.flush().unwrap();
+                        sleep(Duration::from_millis(800));
+                        write!(
+                            stdout,
+                            "\r{}",
+                            clear::CurrentLine,
+                        ).unwrap();
+                        stdout.flush().unwrap();
+                    }
                 }
             }
         }
@@ -159,33 +216,25 @@ async fn main() {
 }
 
 
-async fn interact_with_chat_ai(message: &str) -> String {
+async fn interact_with_chat_ai(message: &str) -> Result<String, String> {
     let client = Client::new();
     let bearer = format!("Bearer {}", std::env::var("CHAT_API_KEY").unwrap());
-    println!("{}", bearer);
-    let mut request: String = r#"
-    {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            {
-                "role": "system",
-                "content": "You are a helpful assistant."
+
+    let request = CompletionRequest {
+        model: "gpt-3.5-turbo".to_owned(),
+        messages: vec![
+            Message {
+                role: "system".to_owned(),
+                content: "You are a helpful assistant.".to_owned()
             },
-            {
-                "role": "user",
-                "content": "{}"
+            Message {
+                role: "user".to_owned(),
+                content: message.to_owned()
             }
-        ]
-    }"#.to_string();
-    request = format!("{}", request.replace("{}", message));
-    // let request = CompletionRequest {
-    //     model: "gpt-3.5-turbo".to_owned(),
-    //     prompt: message.to_owned(),
-    //     max_tokens: 7,
-    //     temperature: 0.7,
-    //     n: 1,
-    //     stream: false,
-    // };
+        ],
+        temperature: 0.7,
+        n: 1,
+    };
 
     let response = client
     .post("https://api.openai.com/v1/chat/completions".to_string())
@@ -193,27 +242,32 @@ async fn interact_with_chat_ai(message: &str) -> String {
     .header("OpenAI-Organization","org-ZFfZMGuCBWpCVT9jmtDaRE5p")
     .header(CONTENT_TYPE, "application/json")
     .header(AUTHORIZATION, &bearer)
-    .body(request)
+    .json(&request)
     .send()
     .await
     .unwrap();
 
-        write!(stdout(), "{:?}\n\r", response).unwrap();
-        stdout().flush().unwrap();
-    let response_json: CompletionResponse = response.json().await.unwrap();
-        write!(stdout(), "{:?}\n\r", response_json).unwrap();
-        stdout().flush().unwrap();
-    let response: Vec<CompletionChoices> = response_json.choices;
-        write!(stdout(), "{:?}\n\r", response).unwrap();
-        stdout().flush().unwrap();
-    if let Some(prob) = &response[0].logprobs {
-        let logprobs: LogProbs = prob.clone();
-        write!(stdout(), "\n\r{}-{}\n\r", logprobs.token_logprobs[0], logprobs.text_offset[0]).unwrap();
-        stdout().flush().unwrap();
-    }
-    response[0].text.clone()
+    match response.status() {
+        reqwest::StatusCode::OK => {
+            match response.json::<CompletionResponse>().await {
+                Ok(parsed) => {
+                    return Ok(parsed.choices[0].message.content.clone());
+                },
+                Err(_) => 
+                    return Err(String::from("🛑 Hm, the response didn't match the shape we expected.")),
+            };
+        }
+        reqwest::StatusCode::UNAUTHORIZED => {
+            return Err(String::from("🛑 Status: UNAUTHORIZED - Need to grab a new token"));
+        }
+        reqwest::StatusCode::TOO_MANY_REQUESTS => {
+            return Err(String::from("🛑 Status: 429 - Too many requests"));
+        }
+        other => {
+            panic!(
+                "🛑 Uh oh! Something unexpected happened: [{:#?}]", 
+                other
+            );
+        }
+    };
 }
-
-Response { url: Url { scheme: "https", cannot_be_a_base: false, username: "", password: None, host: Some(Domain("api.openai.com")), port: None, path: "/v1/chat/completions", query: None, fragment: None }, status: 200, headers: {"date": "Wed, 03 May 2023 02:47:12 GMT", "content-type": "application/json", "content-length": "319", "connection": "keep-alive", "access-control-allow-origin": "*", "cache-control": "no-cache, must-revalidate", "openai-model": "gpt-3.5-turbo-0301", "openai-organization": "user-sr9chuk2z67sv10w9xdptl8j", "openai-processing-ms": "654", "openai-version": "2020-10-01", "strict-transport-security": "max-age=15724800; includeSubDomains", "x-ratelimit-limit-requests": "3", "x-ratelimit-limit-tokens": "40000", "x-ratelimit-remaining-requests": "2", "x-ratelimit-remaining-tokens": "39973", "x-ratelimit-reset-requests": "20s", "x-ratelimit-reset-tokens": "40ms", "x-request-id": "383c8e9b625f2a0c93e158c5288501e9", "cf-cache-status": "DYNAMIC", "server": "cloudflare", "cf-ray": "7c152d89292c27b3-SEA", "alt-svc": "h3=\":443\"; ma=86400, h3-29=\":443\"; ma=86400"} }
-thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: reqwest::Error { kind: Decode, source: Error("missing field `text`", line: 1, column: 316) }', ai-assistant/src/main.rs:203:67
-                                                                               note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
