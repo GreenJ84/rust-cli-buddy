0ff9bd1857efbff1821e8effc39683270721eda1
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-27 22:42:23 -0700

    Update Rust Library testing format

--- buddy-utils/src/lib.rs
+++ buddy-utils/src/lib.rs
@@ -19,39 +19,16 @@ pub fn format_name(program: &str) -> String{
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
-    fn ai_assistant_works() {
-        let result = format_name(&"ai-assistant");
-        assert_eq!(result, String::from("AI Assistant"));
-    }
-    fn calculator_works() {
-        let result = format_name(&"calculator");
-        assert_eq!(result, String::from("Calculator"));
-    }
-    fn development_timer_works() {
-        let result = format_name(&"development-timer");
-        assert_eq!(result, String::from("Development Timer"));
-    }
-    fn file_organizer_works() {
-        let result = format_name(&"file-organizer");
-        assert_eq!(result, String::from("File Organizer"));
-    }
-    fn password_generator_works() {
-        let result = format_name(&"password-generator");
-        assert_eq!(result, String::from("Password Generator"));
-    }
-    fn password_manager_works() {
-        let result = format_name(&"password-manager");
-        assert_eq!(result, String::from("Password Manager"));
-    }
-    fn task_manager_works() {
-        let result = format_name(&"task-manager");
-        assert_eq!(result, String::from("Task Manager"));
-    }
-    fn word_analyzer_works() {
-        let result = format_name(&"word-analyzer");
-        assert_eq!(result, String::from("Word analyzer"));
+    fn format_name_test() {
+        assert_eq!(format_name(&"ai-assistant"), String::from("AI Assistant"));
+        assert_eq!(format_name(&"calculator"), String::from("Calculator"));
+        assert_eq!(format_name(&"development-timer"), String::from("Development Timer"));
+        assert_eq!(format_name(&"file-organizer"), String::from("File Organizer"));
+        assert_eq!(format_name(&"password-generator"), String::from("Password Generator"));
+        assert_eq!(format_name(&"password-manager"), String::from("Password Manager"));
+        assert_eq!(format_name(&"task-manager"), String::from("Task Manager"));
+        assert_eq!(format_name(&"word-analyzer"), String::from("Word analyzer"));
     }
 }
 
