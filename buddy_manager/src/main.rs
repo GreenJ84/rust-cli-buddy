785fd1d7cb94a950908b6fad9e33e9a15210952b
Jesse Greenough <jesse.greenough84@gmail.com>
2024-01-11 00:48:47 -0800

    Refactor buddy_manager program
    Migrate program utilities to library crate
    
    Refactor main functionality within binary crate

--- buddy_manager/src/main.rs
+++ buddy_manager/src/main.rs
@@ -1,141 +1,123 @@
-use std::io::{stdout, stdin, Write};
-use std::process::{Command, Stdio};
-use std::time::Duration;
-use std::thread::sleep;
-use termion::style;
-use termion::cursor::Goto;
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+use dotenv::dotenv;
+use std::io::{stdin, stdout, Write};
+use std::{thread::sleep, time::Duration};
+use termion::{cursor::Goto, event::Key, input::TermRead, raw::IntoRawMode, style};
 
-
-use buddy_service::{database, utils::{self, terminal, application}};
+use buddy_service::{
+    database,
+    utils::{self, application, terminal},
+};
 
 const BUDDY_PROGRAMS: [&str; 8] = [
     "ai-assistant",
-    "calculator", //done, needs Polish
+    "calculator",        //done, needs Polish
     "development-timer", // started
     "file-organizer",
-    "password-manager", // done, needs Polish
+    "password-manager",   // done, needs Polish
     "password-generator", // done, needs Polish
-    "task-manager", 
+    "task-manager",
     "word analyzer",
 ];
 
 fn main() {
+    dotenv().ok();
     // Enter raw control of terminal
-    let mut stdout = stdout().into_raw_mode().unwrap();
-    // Clear all previous terminal dialogue
+    let mut stdout = stdout()
+        .into_raw_mode()
+        .expect("Buddy can't take control of the terminal");
     terminal::clear_terminal(&stdout);
+
     // SQLite DB setup for the application
-    database::database_establishment();
-    // Buddy Manager application entrance
+    if let Err(e) = database::database_establishment() {
+        application::error_confirmation(
+            &mut stdout,
+            &format!("Failed to establish database connection: \n\r\t{:?}\n\rSome applications will be affected", e),
+        );
+    };
     application::enter(&stdout, "Welcome back Buddy!");
 
+    // Program Manager selection loop
     let mut running = true;
     let mut selected = 0;
-    // Program Manager selection loop
     while running {
         let stdin = stdin();
 
         terminal::clear_terminal(&stdout);
         write!(
-            stdout, 
-            "{}{}{}{}{}{}\n", 
+            stdout,
+            "{}{}{}{}{}{}\n",
             style::Bold,
             style::Underline,
             terminal::buddy_text("Select a program to start:"),
             Goto(3, 2),
             terminal::info_text("(enter/'q'uit)"),
             style::Reset
-        ).unwrap();
+        )
+        .unwrap();
         stdout.flush().unwrap();
 
-        // Print the list to Terminal
+        // Print the application list to Terminal
         application::print_programs(&stdout, selected, BUDDY_PROGRAMS.as_slice(), 4);
 
-        // Handle User Input
+        // Handle User Selection Input
         for c in stdin.keys() {
-            match c.unwrap(){
+            match c.unwrap() {
                 Key::Up => {
-                    if selected > 0{
+                    if selected > 0 {
                         selected -= 1;
-                        application::print_programs(&stdout, selected, BUDDY_PROGRAMS.as_slice(), 4);
+                        application::print_programs(
+                            &stdout,
+                            selected,
+                            BUDDY_PROGRAMS.as_slice(),
+                            4,
+                        );
                     }
-                },
+                }
                 Key::Down => {
-                    if selected < BUDDY_PROGRAMS.len() - 1{
+                    if selected < BUDDY_PROGRAMS.len() - 1 {
                         selected += 1;
-                        application::print_programs(&stdout, selected, BUDDY_PROGRAMS.as_slice(), 4);
+                        application::print_programs(
+                            &stdout,
+                            selected,
+                            BUDDY_PROGRAMS.as_slice(),
+                            4,
+                        );
                     }
-                },
+                }
                 Key::Char('\n') => {
                     application::program_selection(&stdout, selected, BUDDY_PROGRAMS.as_slice(), 4);
                     break;
-                },
+                }
                 Key::Char('q') | Key::Esc => {
-                    application::exit(&stdout, "Leaving your buddy behind...", "WHY!?!?!");
                     running = false;
                     break;
-                },
+                }
                 _ => {}
             }
             stdout.flush().unwrap();
         }
-        if !running { break; }
-
-        // Program opening dialogue
-        write!(
-            stdout,
-            "Starting {}", 
-            terminal::buddy_text(&utils::format_name(BUDDY_PROGRAMS[selected]))
-        ).unwrap();
-        stdout.flush().unwrap();
-        sleep(Duration::from_millis(300));
-        terminal::clear_terminal(&stdout);
-
-        spawn_program(BUDDY_PROGRAMS[selected]);
+        if running {
+            // sub-Program opening
+            write!(
+                stdout,
+                "Starting {}",
+                terminal::buddy_text(&utils::format_name(BUDDY_PROGRAMS[selected]))
+            )
+            .unwrap();
+            stdout.flush().unwrap();
+            sleep(Duration::from_millis(300));
+            terminal::clear_terminal(&stdout);
+    
+            buddy_manager::spawn_program(BUDDY_PROGRAMS[selected]);
+        }
     }
+
+    application::exit(&stdout, "Leaving your buddy behind...", "WHY!?!?!");
+    
     terminal::clear_terminal(&stdout);
     terminal::cursor_display(&stdout, true);
 
     // Drop output control
     drop(stdout);
     return;
-}
-
-
-fn spawn_program(program: &str) {
-    // Create a child process
-    let mut command = Command::new("cargo");
-    
-    command
-        .arg("run")
-        .arg("-p")
-        .arg(program)
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit());
-
-    // Spawn child process
-    if let Ok(mut spawn) = 
-        command.spawn() {
-            let exit_status = spawn.wait().expect(&format!("Failed to run {} program.", program));
-            if !exit_status.success() {
-                eprint!("Sub Application exited with an error.");
-            } else {
-                println!(
-                    "\n\r{}",
-                    terminal::info_text(
-                        &format!("Exiting {}. Status: {}",
-                        program, 
-                        exit_status)
-                    )
-                );
-            }
-    } else {
-        eprint!("Program spawning command failed to initialize.");
-    }
-
-    sleep(Duration::from_millis(500));
 }
